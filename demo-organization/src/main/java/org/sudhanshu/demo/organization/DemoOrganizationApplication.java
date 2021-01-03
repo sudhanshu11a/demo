39fd548f98ac0e02573d1ff64397aed617ea7b51
Sudhanshu Shama <sudhanshu11a@gmail.com>
2021-01-03 19:10:45 +0530

    working db encryption password

--- demo-organization/src/main/java/org/sudhanshu/demo/organization/DemoOrganizationApplication.java
+++ demo-organization/src/main/java/org/sudhanshu/demo/organization/DemoOrganizationApplication.java
@@ -1,5 +1,6 @@
 package org.sudhanshu.demo.organization;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -8,6 +9,7 @@ import org.springframework.jms.annotation.EnableJms;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableJms
+@EnableEncryptableProperties
 public class DemoOrganizationApplication {
 
 	public static void main(String[] args) {
